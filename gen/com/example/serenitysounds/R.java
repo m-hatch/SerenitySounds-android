ca68eadc0152017b7c28d0c9552a4eabf449fe8d
m-hatch <montehatch@gmail.com>
2014-08-30 05:55:41 -0400

    edit splash text

--- gen/com/example/serenitysounds/R.java
+++ gen/com/example/serenitysounds/R.java
@@ -22,7 +22,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int relax=0x7f020001;
+        public static final int ic_wave=0x7f020001;
+        public static final int relax=0x7f020002;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f090006;
